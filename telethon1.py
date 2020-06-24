f09cce943410d0b0484f23670e7c33dfededebc7
陈振山 <ieczszzu@qq.com>
2020-06-24 14:39:55 +0800

    新增功能：监听特定频道，自动下载新上传到该频道的文件 --------------- 功能介绍： 1、下载指定频道指定数量的文件（main()函数传入非0整数开启） 2、监听特定频道，自动下载新上传到该频道的文件（默认使用此功能，main()函数传入0或不传参数开启）

--- telethon1.py
+++ telethon1.py
@@ -1,42 +1,45 @@
 import time
-from telethon import TelegramClient
+from telethon import TelegramClient, events
 import asyncio
 
 from myConfig import myapi_id,myapi_hash,myproxy
 
 #-------------------
-channel_name='jianhuangfabu'
-msg_num=22
+channel_name='eggma'
+msg_num=2
 #-------------------
 
 filerelativepath = './downloadFile/'
 client = TelegramClient('test',myapi_id, myapi_hash,proxy=myproxy)
 
-async def main():
+def createTasks(msg,tasks):
+    '''
+    通过msgs创建异步task
+    :param msgs:获取到的消息列表 :TotalList
+    :param tasks:待返回的task列表
+    :return:返回tasks
+    '''
+
+
+    if msg.media != None:
+        if msg.gif != None:
+            filename = filerelativepath + 'gif/'
+        elif msg.photo !=None:
+            filename = filerelativepath + 'photo/'
+        elif msg.video!=None:
+            filename = filerelativepath+'video/'
+        else:
+            return tasks#不是上述三种类型的文件不创建task
+        tasks.append(asyncio.create_task(client.download_media(message=msg, file=filename)))
+    return tasks
+
+async def IwannaDownload():
     global filerelativepath
-    filecnt_video=filecnt_photo=filecnt_gif=1
     msgs =  await client.get_messages(channel_name,limit=msg_num)
     tasks=[]
     for msg in msgs:
-        if msg.media != None:
-            if msg.gif != None:
-                #print('检测到第{}个{}'.format(filecnt_gif, 'gif'))
-                filecnt_gif += 1
-                filename = filerelativepath + 'gif/'
-            elif msg.photo !=None:
-                #print('检测到第{}个{}'.format(filecnt_photo, 'photo'));
-                filecnt_photo+=1
-                filename = filerelativepath + 'photo/'
-            elif msg.video!=None:
-                #print('检测到第{}个{}'.format(filecnt_video, 'video'));
-                filecnt_video+=1
-                filename = filerelativepath+'video/'
-            else:
-                continue
-            tasks.append(asyncio.create_task(client.download_media(message=msg, file=filename)))
-
+        tasks = createTasks(msg, tasks)
     #task创建完成
-    print('共检测到视频：{}个，照片：{}个，gif：{}个'.format(filecnt_video,filecnt_photo,filecnt_gif))
     begin_time = time.time()
     print('开始下载：',begin_time)
     for task in tasks:
@@ -45,6 +48,28 @@ async def main():
     print('下载结束：',end_time)
     print('用时：{}s'.format(end_time-begin_time))
 
-with client:
-    client.loop.run_until_complete(main())
+@client.on(events.NewMessage(chats=channel_name))
+async def DownloadforChannalUpdate(event):
+    media = event.message
+    tasks=[]
+    if media!=None:
+        tasks = createTasks(media,tasks)
+        begin_time = time.time()
+        print('开始下载：', begin_time)
+        for task in tasks:
+            await task
+        end_time = time.time()
+        print('下载结束：', end_time)
+        print('用时：{}s'.format(end_time - begin_time))
+
+def main(arg=0):
+    if arg==0:
+        client.start()
+        client.run_until_disconnected()
+    else:
+        with client:
+            client.loop.run_until_complete(IwannaDownload())
+
+if __name__ == '__main__':
+    main()
 
